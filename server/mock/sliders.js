92ac6b1277c3ebc60b95600e5aa4f995ecc8ae71
liuxuanlife@sina.com <liuxuan3987321>
2017-12-18 17:49:55 +0800

    首页 silder  列表

--- server/mock/sliders.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-    'https://a2.vimage1.com/upload/flow/2017/12/15/136/15133265349090.jpg',
-    'https://a3.vimage1.com/upload/flow/2017/12/15/130/15133264403152.jpg'
-];
---
+++ src/pages/ShopCar/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class ShopCar extends Component{
+    render(){
+        return (
+            <div>
+              Shop
+            </div>
+        )
+    }
+}
