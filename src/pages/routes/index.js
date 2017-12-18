769af19d7b33055b92c37e7888b3a386b84812e8
liuxuanlife@sina.com <liuxuan3987321>
2017-12-18 20:36:47 +0800

    增加购物车,修改routes

--- src/pages/routes/index.js
+++ src/pages/routes/index.js
@@ -3,6 +3,7 @@ import './index.less';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from "../Home";
 import Tab from "../../components/Tab";
+import ShopCar from "../Shop";
 
 export default class App extends Component {
     render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
                 <div className='container'>
                     <Switch>
                         <Route exact path='/' component={Home}/>
+                        <Route  path='/shop' component={ShopCar}/>
                     </Switch>
                     <Tab/>
                 </div>
