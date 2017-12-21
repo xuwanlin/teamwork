57e627f3790ad6fcbe713e6a84aa8ff9d955d570
wangxiaojiao2016 <ffddzcm@foxmail.com>
2017-12-21 11:38:13 +0800

    "search2"

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -5,16 +5,17 @@ import {Link} from 'react-router-dom'
 class Search extends Component {
     render() {
         return (
-            <div>666</div>
+           <div className="search-panel">
+               <Link to={`/brand/${item.category}`}>
+                   <Link className="search-jys">娇韵诗</Link>
+                   <Link className="search-zrt">自然堂</Link>
+                   <Link className="search-ksy">科颜氏</Link>
+                   <Link className="search-shk">施华蔻</Link>
+                   <Link className="search-ynf">御泥坊</Link>
+                   <Link className="search-sk">SK-II</Link>
+
+               </Link>
+           </div>
         )
     }
 }
-/*<Link to={`/brand/${item.category}`} className="search-panel">
-                <Link className="search-jys">娇韵诗</Link>
-                <Link className="search-zrt">自然堂</Link>
-                <Link className="search-ksy">科颜氏</Link>
-                <Link className="search-shk">施华蔻</Link>
-                <Link className="search-ynf">御泥坊</Link>
-                <Link className="search-sk">SK-II</Link>
-
-            </Link>*/
