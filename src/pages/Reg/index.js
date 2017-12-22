acc9e58fac9784105d156e3b60adef9adfb5a572
liuxuanlife@sina.com <liuxuan3987321>
2017-12-22 17:57:54 +0800

    个人中心

--- src/pages/Reg/index.js
+++ src/pages/Reg/index.js
@@ -2,37 +2,45 @@ import React, {Component} from 'react';
 import './index.less';
 import Mheader from "../../components/Mheader";
 import {post} from '../../api/index';
+
 export default class Login extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {username:'',password:''};
+        this.state = {username: '', password: '', error: ''};
     }
+
     reg = () => {
         let username = this.username.value;
         let password = this.password.value;
         post('/api/reg',
-        {
+            {
                 username,
                 password
-        }).then(res => {
+            }).then(res => {
             if (res.code === 0) {
-                window.location.href = "/#/login";
+                this.props.history.push('/login');
+            } else {
+                this.setState({error: res.error});
             }
         });
-    }
+    };
+
     render() {
         return (
-          <div>
-            <Mheader title="注册"/>
-            <div className="login-panel">
-              <div className="login-logo">
-                <img src={require('../../common/images/2.jpg')} alt=""/>
-              </div>
-              <input ref={input=>this.username=input} type="text" placeholder="手机号"/>
-              <input ref={input=>this.password=input} type="text" placeholder="密码"/>
-              <div onClick={this.reg} className="login-button">注&nbsp;册</div>
+            <div>
+                <Mheader title="注册"/>
+                <div className="login-panel">
+                    <div className="login-logo">
+                        <img src={require('../../common/images/2.jpg')} alt=""/>
+                    </div>
+                    <input ref={input => this.username = input} type="text" placeholder="手机号"/>
+                    <input ref={input => this.password = input} type="text" placeholder="密码"/>
+                    <div onClick={this.reg} className="login-button">注&nbsp;册</div>
+                    {
+                        this.state.error && <div className='tips'>{this.state.error}</div>
+                    }
+                </div>
             </div>
-          </div>
-        )
+        );
     }
 }
